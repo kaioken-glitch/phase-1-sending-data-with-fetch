8b1b9301bb532a3e950bf399659555297a2be352
Cyprian <lightningcyranus@gmail.com>
2025-06-19 19:55:32 +0200

    first APIU still learning streamlining

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+function submitData(name, email){
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    })
+    .then(res => res.json())
+    .then(data => {
+        const newID = document.createElement('div')
+        newID.textContent = data.id;
+        document.body.appendChild(newID);
+        return data;
+    })
+    .catch(error => {
+        const errorMsg = document.createElement('div');
+        errorMsg.textContent = error.message;
+        document.body.appendChild(errorMsg);
+    });
+}
+
+submitData()
